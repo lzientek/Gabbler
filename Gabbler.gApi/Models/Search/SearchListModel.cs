74651ffc2e63372f2819490efff693fbf32eff67
lzientek <152565@supinfo.com>
2015-03-07 18:07:20 +0000

    Search api fonctionnel

---
+++ Gabbler.gApi/Models/Search/SearchListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gabbler.gApi.Models.Gabs;
+using Gabbler.gApi.Models.Users;
+
+namespace Gabbler.gApi.Models.Search
+{
+    public class SearchListModel
+    {
+        public IEnumerable<UserBasicModel> ListOfUser { get; set; }
+        public GabsList ListOfGab { get; set; }
+
+
+
+    }
+}
