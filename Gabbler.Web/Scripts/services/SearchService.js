f0f974d6bdcbcd5ee5e0f78a4c3ad7c5851c3293
woltek <schmitterarnaud@gmail.com>
2015-03-16 11:14:24 +0000

    fail

---
+++ Gabbler.Web/Scripts/services/SearchService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('searchService', ['$http', function ($http) {
+
+    var gabServiceFactory = {};
+
+    var _getSearch = function () {
+
+        return $http.get(serviceBase + 'gabs').then(function (results) {
+            return results;
+        });
+    };
+
+    gabServiceFactory.getSearch = _getSearch;
+
+
+    return gabServiceFactory;
+
+}]);
